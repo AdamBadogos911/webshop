e0dd339f47ccb71d058e7f73e92f4f0d1a4a2cc3
AdamBadogos911 <badogos.adam911@gmail.com>
2025-11-16 18:05:24 +0100

    mappák létrehozása

---
+++ webshop/src/app/components/opened-navbar/opened-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-opened-navbar',
+  imports: [],
+  templateUrl: './opened-navbar.html',
+  styleUrl: './opened-navbar.css',
+})
+export class OpenedNavbar {
+
+}
---
+++ webshop/src/app/components/storage-card/storage-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-storage-card',
+  imports: [],
+  templateUrl: './storage-card.html',
+  styleUrl: './storage-card.css',
+})
+export class StorageCard {
+
+}
