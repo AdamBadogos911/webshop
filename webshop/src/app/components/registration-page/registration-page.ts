e719e17820816b57a3d28043f450daa06d76832e
AdamBadogos911 <badogos.adam911@gmail.com>
2025-11-21 11:12:31 +0100

    Registráció design, kis funkciók

--- webshop/src/app/components/registration-page/registration-page.ts
+++ webshop/src/app/components/registration-page/registration-page.ts
@@ -7,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrl: './registration-page.css',
 })
 export class RegistrationPage {
+  enteredLastName = '';
+  enteredFirstName = '';
+  enteredEmail = '';
+  enteredPassword = '';
+  enteredPasswordAgain = '';
 
+  onSubmit() {
+    if (
+      !this.enteredLastName ||
+      !this.enteredFirstName ||
+      !this.enteredEmail ||
+      !this.enteredPassword ||
+      !this.enteredPasswordAgain
+    ) {
+      alert('Kérem töltsön ki minden mezőt!');
+    } else if (
+      this.enteredEmail.includes('@') === false ||
+      this.enteredEmail.includes('.') === false
+    ) {
+      alert('Kérem érvényes email címet adjon meg!');
+    } else if (this.enteredPassword !== this.enteredPasswordAgain) {
+      alert('A jelszavak nem egyeznek!');
+    } else {
+      alert('Sikeres regisztráció!');
+      console.log(this.enteredLastName);
+      console.log(this.enteredFirstName);
+      console.log(this.enteredEmail);
+      console.log(this.enteredPassword);
+    }
+  }
 }
