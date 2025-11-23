03c2fd8e1bc38bacdd5d14229371bb108750d848
AdamBadogos911 <badogos.adam911@gmail.com>
2025-11-23 17:51:26 +0100

    login oldal

--- webshop/src/app/app.ts
+++ webshop/src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RegistrationPage } from "./components/registration-page/registration-page";
 import { Navbar } from "./components/navbar/navbar";
+import { LoginPage } from "./components/login-page/login-page";
 
 @Component({
   selector: 'app-root',
-  imports: [RegistrationPage, Navbar],
+  imports: [RegistrationPage, Navbar, LoginPage],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
