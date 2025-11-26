76244ddfcf5cec6f3a1d016304111304bdc35c60
AdamBadogos911 <badogos.adam911@gmail.com>
2025-11-26 18:51:18 +0100

    Bejelentkezés folyamat lefejlesztése

--- webshop/src/app/components/login-page/login-page.ts
+++ webshop/src/app/components/login-page/login-page.ts
@@ -1,12 +1,35 @@
 import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { UserService } from '../../services/user-service';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [RouterModule, ReactiveFormsModule],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
+  private userService = inject(UserService);
+  private router = inject(Router);
+  loginForm!: FormGroup;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(8)])
+    })
+  }
+
+  login() {
+    this.userService.login(this.loginForm.controls["email"].value, this.loginForm.controls["password"].value).subscribe({
+      next: response => {
+        console.log(response)
+      }, error: error => {
+
+      }, complete: () => {
+        this.router.navigate(["/homePage"])
+      }
+    })
+  }
 }
