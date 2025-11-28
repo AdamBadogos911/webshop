cc1551425a4a95fe89de785c9962c400b1e1b5cc
martingamer07 <gardony0901martin@gmail.com>
2025-11-28 09:13:43 +0100

    AddressType
    entity class kész

---
+++ badogosShop/src/main/java/com/example/badogosShop/entity/AddressType.java
@@ -0,0 +1,36 @@
+package com.example.badogosShop.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.antlr.v4.runtime.misc.NotNull;
+
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Entity
+@Table(name = "address_type")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class AddressType {
+
+        @Id
+        @GeneratedValue
+        @Column(name = "id")
+        private Integer id;
+
+        @Column(name = "name")
+        @NotNull
+        @Size(max = 100)
+        private String name;
+
+        @OneToMany(mappedBy = "billingAddressType", fetch = FetchType.LAZY, cascade = {})
+        @JsonIgnore
+        private List<TransportDetail> transportDetails;
+}
