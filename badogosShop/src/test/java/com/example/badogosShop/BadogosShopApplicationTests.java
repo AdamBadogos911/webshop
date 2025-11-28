bcf7cfd7391407ab60ac5f59fce160e967cc57cb
martingamer07 <gardony0901martin@gmail.com>
2025-11-28 08:15:51 +0100

    projekt létrehozása

---
+++ badogosShop/src/test/java/com/example/badogosShop/BadogosShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.badogosShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BadogosShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
