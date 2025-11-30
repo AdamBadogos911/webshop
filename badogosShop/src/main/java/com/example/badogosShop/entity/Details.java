b68d12eda74f3701801201fe857ffa7f7ff8b869
martingamer07 <gardony0901martin@gmail.com>
2025-11-30 21:21:44 +0100

    Details class
    entity class kész

---
+++ badogosShop/src/main/java/com/example/badogosShop/entity/Details.java
@@ -0,0 +1,59 @@
+package com.example.badogosShop.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.validation.constraints.Null;
+
+@Entity
+@Table(name = "details")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+
+public class Details {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "weight")
+    @Null
+    private Double weightInKg;
+
+    @Column(name = "species")
+    @Null
+    private String species;
+
+    @Column(name = "length")
+    @Null
+    private Double lengthInCm;
+
+    @Column(name = "height")
+    @Null
+    private Double heightInCm;
+
+    @Column(name = "width")
+    @Null
+    private Double widthInCm;
+
+    @Column(name = "size")
+    @Null
+    private Integer size;
+
+    @Column(name = "is_set")
+    @Null
+    private Boolean isSet;
+
+    @OneToOne(mappedBy = "detail", cascade = {})
+    @JsonIgnore
+    private Product product;
+
+
+}
