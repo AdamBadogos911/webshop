114c881d91712104631f6174c1afe9137ebd1998
AdamBadogos911 <badogos.adam911@gmail.com>
2025-12-01 10:25:00 +0100

    App route, navbar profil gomb

--- webshop/src/app/app.routes.ts
+++ webshop/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomePage } from './components/home-page/home-page';
+import { LoginPage } from './components/login-page/login-page';
+import { RegistrationPage } from './components/registration-page/registration-page';
+import { PasswordResetPage } from './components/password-reset-page/password-reset-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "homePage", component: HomePage, },
+  { path: "", pathMatch: "full", redirectTo: "homePage" },
+  { path: "login", component: LoginPage },
+  { path: "register", component: RegistrationPage },
+  { path: "passwordReset", component: PasswordResetPage }
+];
--- webshop/src/app/app.ts
+++ webshop/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RegistrationPage } from "./components/registration-page/registration-page";
 import { Navbar } from "./components/navbar/navbar";
 import { LoginPage } from "./components/login-page/login-page";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [RegistrationPage, Navbar, LoginPage],
+  imports: [RegistrationPage, Navbar, LoginPage, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
