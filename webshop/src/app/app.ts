dbb8b538cf276d9fa9b8f532349cb9f9cc4fec28
AdamBadogos911 <badogos.adam911@gmail.com>
2025-12-02 13:45:24 +0100

    Footer

--- webshop/src/app/app.ts
+++ webshop/src/app/app.ts
@@ -3,10 +3,11 @@ import { RegistrationPage } from "./components/registration-page/registration-pa
 import { Navbar } from "./components/navbar/navbar";
 import { LoginPage } from "./components/login-page/login-page";
 import { RouterOutlet } from "@angular/router";
+import { Footer } from "./components/footer/footer";
 
 @Component({
   selector: 'app-root',
-  imports: [RegistrationPage, Navbar, LoginPage, RouterOutlet],
+  imports: [RegistrationPage, Navbar, LoginPage, RouterOutlet, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
