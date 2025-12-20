05a4f5394afe0a6696805e9dc12209e459cf84af
AdamBadogos911 <badogos.adam911@gmail.com>
2025-12-20 15:21:20 +0100

    Termékek fül

--- webshop/src/app/components/navbar/navbar.ts
+++ webshop/src/app/components/navbar/navbar.ts
@@ -2,10 +2,11 @@ import { User } from './../../models/user.model';
 import { Component, inject } from '@angular/core';
 import { RouterLink } from "@angular/router";
 import { UserService } from '../../services/user-service';
+import { ProductList } from "../product-list/product-list";
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink],
+  imports: [RouterLink, ProductList],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
