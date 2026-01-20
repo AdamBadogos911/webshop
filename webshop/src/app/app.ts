4c51e30978678f283ca0ad1c9fa81f2a77fa00df
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-20 19:56:17 +0100

    App bugfix

--- webshop/src/app/app.ts
+++ webshop/src/app/app.ts
@@ -1,17 +1,15 @@
 import { Component, signal } from '@angular/core';
-import { RegistrationPage } from "./components/registration-page/registration-page";
-import { Navbar } from "./components/navbar/navbar";
-import { LoginPage } from "./components/login-page/login-page";
 import { RouterOutlet } from "@angular/router";
 import { Footer } from "./components/footer/footer";
-import { ProductList } from "./components/product-list/product-list";
+import { Navbar } from "./components/navbar/navbar";
+import { OpenedNavbar } from './components/opened-navbar/opened-navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RegistrationPage, Navbar, LoginPage, RouterOutlet, Footer, ProductList],
+  imports: [ Navbar, RouterOutlet, Footer, OpenedNavbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('webshop');
+  isNavbarOpen = signal<boolean>(false)
 }
