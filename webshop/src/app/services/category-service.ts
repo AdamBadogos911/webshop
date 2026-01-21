a4ff018d0268d3db9b4434d9b904f4a9155e9f15
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-21 14:27:08 +0100

    Category service

--- webshop/src/app/services/category-service.ts
+++ webshop/src/app/services/category-service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Category } from '../models/category.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CategoryService {
-  
+  private http = inject(HttpClient);
+  private baseURL = "http://localhost:8080/category";
+
+  getAllMainCategory(): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.baseURL}/main`)
+  }
+
+  getAllSubCategoryFromMainCategory(mainCategoryId: number): Observable<Category[]> {
+    return this.http.get<Category[]>(`${this.baseURL}/main/${mainCategoryId}/sub`)
+  }
 }
