92e2899e514592ff149ad60776ca0b11aaed0067
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-23 08:31:47 +0100

    Leugró termékek fül

--- webshop/src/app/components/opened-navbar/opened-navbar.ts
+++ webshop/src/app/components/opened-navbar/opened-navbar.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from '../../services/category-service';
+import { Category } from '../../models/category.model';
 
 @Component({
   selector: 'app-opened-navbar',
@@ -6,6 +8,21 @@ import { Component } from '@angular/core';
   templateUrl: './opened-navbar.html',
   styleUrl: './opened-navbar.css',
 })
-export class OpenedNavbar {
+export class OpenedNavbar implements OnInit {
+  categoryService = inject(CategoryService)
+  mainCategories: Category[] = []
+  subCategories: Category[] = []
 
+  ngOnInit(): void {
+    this.categoryService.getAllMainCategory().subscribe({
+      next: response => this.mainCategories = response
+    })
+  }
+
+  getAllSubCategory(mainCategoryId: number) {
+    console.log(mainCategoryId)
+    this.categoryService.getAllSubCategoryFromMainCategory(mainCategoryId).subscribe({
+      next: response => this.subCategories = response
+    })
+  }
 }
