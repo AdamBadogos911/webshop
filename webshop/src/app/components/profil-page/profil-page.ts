f2869f38221775500ab34ed0d293b09b9ad6e290
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-26 10:21:14 +0100

    Nem használt importok kiszedése

--- webshop/src/app/components/profil-page/profil-page.ts
+++ webshop/src/app/components/profil-page/profil-page.ts
@@ -1,8 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { UserService } from '../../services/user-service';
-import { ReviewCard } from '../review-card/review-card';
 import { Review } from '../../models/review.model';
-import { OrderCard } from '../order-card/order-card';
 import { OrderHistory } from '../../models/orderHistory.model';
 import { AddressUser } from '../../models/addressUser.model';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
