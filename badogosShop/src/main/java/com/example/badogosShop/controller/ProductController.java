7e6a7ec369329a9f43e34fbda245065cecc26e2b
martingamer07 <gardony0901martin@gmail.com>
2026-01-26 20:53:55 +0100

    BS-87
    ProductController megírva

--- badogosShop/src/main/java/com/example/badogosShop/controller/ProductController.java
+++ badogosShop/src/main/java/com/example/badogosShop/controller/ProductController.java
@@ -2,8 +2,10 @@ package com.example.badogosShop.controller;
 
 import com.example.badogosShop.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("/product")
@@ -12,5 +14,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
 
     private ProductService productService;
+
+    @GetMapping("/category/{id}")
+        public ResponseEntity<Object> getProductByCategory(Pageable pageable, @PathVariable("id") Integer categoryId) {
+            return productService.getProductCategory(pageable, categoryId);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Object> deleteProduct(@PathVariable("id") Integer id) {
+        return productService.deleteProduct(id);
+    }
+
+    @GetMapping("")
+    public ResponseEntity<Object> getAllProduct(Pageable pageable) {
+        return productService.getAllProduct(pageable);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Object> getProductById(@PathVariable("id") Integer id) {
+        return productService.getProductById(id);
+    }
 
 }
