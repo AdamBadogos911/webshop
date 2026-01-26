5ab41e699831073668eeb92ca28ead09902c31ea
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-26 10:16:12 +0100

    Termékek

--- webshop/src/app/components/product-list/product-list.ts
+++ webshop/src/app/components/product-list/product-list.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../models/product.mode';
+import { ProductCard } from '../product-card/product-card';
 
 @Component({
   selector: 'app-product-list',
-  imports: [],
+  imports: [ProductCard],
   templateUrl: './product-list.html',
   styleUrl: './product-list.css',
 })
-export class ProductList {
+export class ProductList implements OnInit {
+  private route = inject(ActivatedRoute)
+  productService = inject(ProductService)
+  productList: Product[] = []
+  isError: boolean = false
 
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: param => {
+        const categoryId: number = param["categoryId"]
+        this.productService.getProductByCategory(categoryId).subscribe({
+          next: response => this.productList = response,
+          error: error => this.isError = true
+        })
+      }
+    })
+  }
+
+  getRows(): Product[][] {
+    const rows: Product[][] = []
+    for (let i: number = 0; i < this.productList.length; i+=4) {
+      const row: Product[] = []
+      for (let j: number = i; j < i+4; j++) {
+        if (this.productList[j] != undefined) {
+          row.push(this.productList[j])
+        }
+      }
+      rows.push(row)
+    }
+
+    return rows;
+  }
 }
