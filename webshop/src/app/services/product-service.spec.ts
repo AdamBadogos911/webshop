13a28adc8a0be10b2aa7c2d11f618572943be6b7
AdamBadogos911 <badogos.adam911@gmail.com>
2026-01-26 09:54:58 +0100

    Product Service

--- webshop/src/app/services/product-service.spec.ts
+++ webshop/src/app/services/product-service.spec.ts
@@ -1,16 +1,15 @@
-import { TestBed } from '@angular/core/testing';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Product } from '../models/product.mode';
 
-import { ProductService } from './product-service';
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private http = inject(HttpClient)
+  private baseUrl = "http://localhost:8080/product"
 
-describe('ProductService', () => {
-  let service: ProductService;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(ProductService);
-  });
-
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  getProductByCategory(categoryId: number) {
+    return this.http.get<Product[]>(`${this.baseUrl}/category/${categoryId}?page=0&size=20`)
+  }
+}
