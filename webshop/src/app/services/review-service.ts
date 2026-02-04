1a0e67eb69ce25da860d31ac434934e6e9584146
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-04 15:15:50 +0100

    Review Service

--- webshop/src/app/services/review-service.ts
+++ webshop/src/app/services/review-service.ts
@@ -1,8 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Review } from '../models/review.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ReviewService {
-  
+  private http = inject(HttpClient)
+  private baseUrl: string = "http://localhost:8080/review"
+
+  getReviewsByUser(userId: number): Observable<Review[]> {
+    return this.http.get<Review[]>(`${this.baseUrl}/user/${userId}`)
+  }
+
+  getReviewsAboutProduct(productId: number): Observable<Review[]> {
+    return this.http.get<Review[]>(`${this.baseUrl}/product/${productId}`)
+  }
 }
