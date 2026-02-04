1cac4ec00fbfca89b48577e3a19a748807912115
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-04 16:03:45 +0100

    Profil fül

--- webshop/src/app/components/profil-page/profil-page.ts
+++ webshop/src/app/components/profil-page/profil-page.ts
@@ -4,27 +4,39 @@ import { Review } from '../../models/review.model';
 import { OrderHistory } from '../../models/orderHistory.model';
 import { AddressUser } from '../../models/addressUser.model';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ReviewService } from '../../services/review-service';
+import { ReviewCard } from '../review-card/review-card';
+import { OrderService } from '../../services/order-service';
+import { OrderCard } from '../order-card/order-card';
 
 @Component({
   selector: 'app-profil-page',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, ReviewCard, OrderCard],
   templateUrl: './profil-page.html',
   styleUrl: './profil-page.css',
 })
 export class ProfilPage implements OnInit {
   userService = inject(UserService)
-  selectedContent: "profilPage" | "orderHistory" | "reviews" | "addresses" = "profilPage"
+  reviewService = inject(ReviewService)
+  orderService = inject(OrderService)
+  selectedContent: "profilPage" | "orderHistory" | "reviews" = "profilPage"
   reviewList: Review[] = []
-  orderHistory: OrderHistory[] = []
+  orderHistories: OrderHistory[] = []
   addressList: AddressUser[] = []
 
   updateForm!: FormGroup
 
   ngOnInit(): void {
-
+    this.selectedContent = "profilPage"
+    this.updateForm = new FormGroup({
+      firstName: new FormControl("", [Validators.required]),
+      lastName: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required, Validators.email]),
+      phoneNumber: new FormControl("", [Validators.required])
+    })
   }
 
-  selectContent(newContent: "profilPage" | "orderHistory" | "reviews" | "addresses") {
+  selectContent(newContent: "profilPage" | "orderHistory" | "reviews") {
     this.selectedContent = newContent
     if (this.selectedContent == "profilPage") {
       this.updateForm = new FormGroup({
@@ -33,7 +45,7 @@ export class ProfilPage implements OnInit {
         lastName: new FormControl("", [Validators.required]),
         phoneNumber: new FormControl("", [Validators.required])
       })
-    } else if (this.selectedContent == "orderHistory"){
+    } else if (this.selectedContent == "orderHistory") {
       this.getOrderHistory()
     } else if (this.selectedContent == "reviews") {
       this.getReviews()
@@ -41,11 +53,17 @@ export class ProfilPage implements OnInit {
   }
 
   getOrderHistory() {
-
+    this.orderService.getOrderHistoryByUserId(this.userService.user?.id!).subscribe({
+      next: response => this.orderHistories = response
+    })
   }
 
   getReviews() {
-
+    this.reviewService.getReviewsByUser(this.userService.user?.id!).subscribe({
+      next: response => {
+        this.reviewList = response
+      }
+    })
   }
 
   deleteUser() {
