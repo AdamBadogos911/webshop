6f11112433e63e0d4f8a81cd0a675771cdad3d6b
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-04 16:02:26 +0100

    Order Service

--- webshop/src/app/services/order-service.ts
+++ webshop/src/app/services/order-service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { OrderHistory } from '../models/orderHistory.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OrderService {
-  
+  private http = inject(HttpClient)
+  private baseUrl = "http://localhost:8080/order"
+
+  getOrderHistoryByUserId(userId: number): Observable<OrderHistory[]> {
+    return this.http.get<OrderHistory[]>(`${this.baseUrl}/user/${userId}`)
+  }
 }
