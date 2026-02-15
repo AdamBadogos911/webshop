d21dddb6501e8ee65c71b9f8da2daf7cce678b50
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-15 16:18:47 +0100

    Product Service

--- webshop/src/app/services/product-service.ts
+++ webshop/src/app/services/product-service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product } from '../models/product.mode';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +10,11 @@ export class ProductService {
   private http = inject(HttpClient)
   private baseUrl = "http://localhost:8080/product"
 
-  getProductByCategory(categoryId: number) {
+  getProductByCategory(categoryId: number): Observable<Product[]> {
     return this.http.get<Product[]>(`${this.baseUrl}/category/${categoryId}?page=0&size=20`)
+  }
+
+  getProductById(productId: number): Observable<Product> {
+    return this.http.get<Product>(`${this.baseUrl}/${productId}`)
   }
 }
