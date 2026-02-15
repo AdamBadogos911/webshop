d2a0c0ad12adb00ca7af20149bd0c7095d94dee2
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-15 16:20:06 +0100

    Egy termék oldal html + script

--- webshop/src/app/components/product-details/product-details.ts
+++ webshop/src/app/components/product-details/product-details.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ReviewService } from '../../services/review-service';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../../services/product-service';
+import { Product } from '../../models/product.mode';
+import { Review } from '../../models/review.model';
+import { ReviewCard } from '../review-card/review-card';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [ReviewCard],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
-export class ProductDetails {
+export class ProductDetails implements OnInit{
+  reviewService = inject(ReviewService)
+  productService = inject(ProductService)
+  route = inject(ActivatedRoute)
+  showReviews = false
+  selectedProduct!: Product
+  reviews: Review[] = []
 
+  ngOnInit(): void {
+    this.route.params.subscribe({
+      next: params => {
+        this.productService.getProductById(params["productId"]).subscribe({
+          next: response => {
+            this.selectedProduct = response
+          }
+        })
+      }
+    })
+  }
+
+  handleReview(isShowReview: boolean) {
+    this.showReviews = isShowReview
+    if (this.showReviews) {
+      this.getReviewsAboutProduct()
+    } else {
+      this.reviews = []
+    }
+  }
+
+  getReviewsAboutProduct() {
+    this.reviewService.getReviewsAboutProduct(this.selectedProduct.id!).subscribe({
+      next: response => {
+        this.reviews = response
+      }
+    })
+  }
 }
