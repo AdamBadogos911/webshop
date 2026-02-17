1696ab307b9e33238b498a49fa8e21f837c25026
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 09:27:16 +0100

    Other service

--- webshop/src/app/services/other-service.ts
+++ webshop/src/app/services/other-service.ts
@@ -1,8 +1,21 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { PaymentMethod } from '../models/paymentMethod.model';
+import { AddressType } from '../models/addressType.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OtherService {
-  
+  private http = inject(HttpClient)
+  private baseUrl: string = "http://localhost:8080"
+
+  getAllPaymentMethod(): Observable<PaymentMethod[]> {
+    return this.http.get<PaymentMethod[]>(`${this.baseUrl}/paymentMethods`)
+  }
+
+  getAllAddressType(): Observable<AddressType[]> {
+    return this.http.get<AddressType[]>(`${this.baseUrl}/addressType`)
+  }
 }
