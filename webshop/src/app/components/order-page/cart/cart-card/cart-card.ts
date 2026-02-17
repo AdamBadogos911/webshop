2aad21584e2d2d08f212c04c7092efd16ffa2c10
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 09:54:36 +0100

    Kosár kártya létrehozása

---
+++ webshop/src/app/components/order-page/cart/cart-card/cart-card.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cart-card',
+  imports: [],
+  templateUrl: './cart-card.html',
+  styleUrl: './cart-card.css',
+})
+export class CartCard {
+
+}
