7539a9d8a978c305b982384b23750e46ab4e6c15
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 12:43:10 +0100

    Cart kártya HTML + script

--- webshop/src/app/components/order-page/cart/cart-card/cart-card.ts
+++ webshop/src/app/components/order-page/cart/cart-card/cart-card.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, input, output } from '@angular/core';
+import { CartService } from '../../../../services/cart-service';
+import { UserService } from '../../../../services/user-service';
+import { CartProduct } from '../../../../models/cartProduct.model';
 
 @Component({
   selector: 'app-cart-card',
@@ -7,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './cart-card.css',
 })
 export class CartCard {
+  cartProduct = input.required<CartProduct>()
+  cartService = inject(CartService)
+  userService = inject(UserService)
+  changeAmount = output<number>()
 
+  changeAmountOfProduct(plusValue: 1 | -1) {
+    this.cartService.changeAmountOfProduct(this.userService.user?.id!, this.cartProduct().id!, this.cartProduct().amount + plusValue).subscribe({
+      next: response => {
+        if (this.cartProduct().amount + plusValue === 0) {
+          this.cartService.usersCart.cartProductList = this.cartService.usersCart.cartProductList?.filter((cp) => cp.id != this.cartProduct().id)
+        } else {
+          this.changeAmount.emit(this.cartProduct().amount + plusValue)
+        }
+      }
+    })
+  }
+
+  deleteProductFromBasket() {
+    this.cartService.deleteProductFromCart(this.cartProduct().id!, this.userService.user?.id!).subscribe({
+      next: response => {
+        this.cartService.usersCart.cartProductList = this.cartService.usersCart.cartProductList?.filter((cp) => cp.id != this.cartProduct().id)
+      }
+    })
+  }
 }
