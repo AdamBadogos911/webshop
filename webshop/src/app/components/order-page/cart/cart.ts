1811792904b1cd1c1dee0cffef32959d5db953ac
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 12:44:06 +0100

    Kosár HTML + script

--- webshop/src/app/components/order-page/cart/cart.ts
+++ webshop/src/app/components/order-page/cart/cart.ts
@@ -1,11 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CartService } from '../../../services/cart-service';
+import { UserService } from '../../../services/user-service';
+import { CartCard } from './cart-card/cart-card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
-  imports: [],
+  imports: [CartCard, RouterModule],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
-export class Cart {
+export class CartPage implements OnInit{
+  cartService = inject(CartService)
+  userService = inject(UserService)
 
+  ngOnInit(): void {
+    this.cartService.getCartByUserId(this.userService.user?.id!).subscribe({
+      next: response => {
+        this.cartService.usersCart = response
+      }
+    })
+  }
+
+  deleteProductFromCart() {
+
+  }
+
+  changeAmount(newAmount: number, index: number) {
+    const searchedProduct = this.cartService.usersCart.cartProductList![index]
+    searchedProduct.amount = newAmount
+    this.cartService.usersCart.cartProductList![index] = searchedProduct
+  }
+
+  clearCart() {
+
+  }
 }
