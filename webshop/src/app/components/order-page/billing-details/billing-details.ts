46d82744ca62407d6a3d5495b24ed3cf8a909db7
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 09:48:48 +0100

    Váráslás végigviteléhez szükséges komponensek létrehozása, elrendezése

---
+++ webshop/src/app/components/order-page/billing-details/billing-details.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-billing-details',
+  imports: [],
+  templateUrl: './billing-details.html',
+  styleUrl: './billing-details.css',
+})
+export class BillingDetails {
+
+}
