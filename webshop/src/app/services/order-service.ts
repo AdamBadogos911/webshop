8c29903fae414b700b37ee52cc6762a88b029d18
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 09:26:54 +0100

    Order service update

--- webshop/src/app/services/order-service.ts
+++ webshop/src/app/services/order-service.ts
@@ -9,8 +9,13 @@ import { Observable } from 'rxjs';
 export class OrderService {
   private http = inject(HttpClient)
   private baseUrl = "http://localhost:8080/order"
+  actualOrder: OrderHistory = new OrderHistory()
 
   getOrderHistoryByUserId(userId: number): Observable<OrderHistory[]> {
     return this.http.get<OrderHistory[]>(`${this.baseUrl}/user/${userId}`)
+  }
+
+  sendOrder(cartId: number) {
+    return this.http.post(`${this.baseUrl}/cart/${cartId}`, this.actualOrder)
   }
 }
