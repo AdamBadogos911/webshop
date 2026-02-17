ef69db8255138b072e6b3b1b3e37685f4123559b
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 10:41:02 +0100

    Cart service

--- webshop/src/app/services/cart-service.ts
+++ webshop/src/app/services/cart-service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Cart } from '../models/cart.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  
+  private http = inject(HttpClient)
+  private baseUrl: string = "http://localhost:8080/cart"
+  usersCart!: Cart
+
+  getCartByUserId(userId: number): Observable<Cart> {
+    return this.http.get<Cart>(`${this.baseUrl}/user/${userId}`)
+  }
+
+  deleteProductFromCart(cartProductId: number, userId: number) {
+    return this.http.delete(`${this.baseUrl}/product?cartProductId=${cartProductId}&userId=${userId}`)
+  }
+
+  changeAmountOfProduct(userId: number, cartProductId: number, amount: number) {
+    return this.http.patch(`${this.baseUrl}/${userId}`, {
+      productId: cartProductId,
+      newAmount: amount
+    })
+  }
+
+  addProductToBasket(userId: number, productId: number, amount: number) {
+    return this.http.post(`${this.baseUrl}/${userId}`, {productId: productId, amount: amount})
+  }
 }
