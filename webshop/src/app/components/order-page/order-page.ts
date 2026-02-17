802ddb6177ff1949409076ec76b58dc78288df07
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-17 09:52:09 +0100

    Order Page

--- webshop/src/app/components/order-page/order-page.ts
+++ webshop/src/app/components/order-page/order-page.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-order-page',
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './order-page.html',
   styleUrl: './order-page.css',
 })
