1c4ea86a6007d7b37c52816eaeff2087d19cd8d5
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-18 10:13:20 +0100

    Summary Page HTML + script

--- webshop/src/app/components/order-page/summary-page/summary-page.ts
+++ webshop/src/app/components/order-page/summary-page/summary-page.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../services/order-service';
+import { CartService } from '../../../services/cart-service';
+import { UserService } from '../../../services/user-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-summary-page',
@@ -6,6 +10,26 @@ import { Component } from '@angular/core';
   templateUrl: './summary-page.html',
   styleUrl: './summary-page.css',
 })
-export class SummaryPage {
+export class SummaryPage implements OnInit {
+  orderService = inject(OrderService)
+  cartService = inject(CartService)
+  userService = inject(UserService)
+  transportAddress: string = ""
+  billingAddress: string = ""
+  router = inject(Router)
 
+  ngOnInit(): void {
+    this.transportAddress = this.orderService.actualOrder.orderTransportDetail?.postCode + " " + this.orderService.actualOrder.orderTransportDetail?.town + this.orderService.actualOrder.orderTransportDetail?.address + this.orderService.actualOrder.orderTransportDetail?.houseNumber
+    this.billingAddress = this.orderService.actualOrder.orderBillingDetail?.postCode + " " + this.orderService.actualOrder.orderBillingDetail?.town + this.orderService.actualOrder.orderBillingDetail?.address + this.orderService.actualOrder.orderBillingDetail?.houseNumber
+  }
+
+  sendOrder() {
+    this.orderService.actualOrder.orderUser = this.userService.user!
+    this.orderService.sendOrder(this.cartService.usersCart.id!).subscribe({
+      next: response => console.log(response),
+      complete: () => {
+        this.router.navigate(["/home"])
+      }
+    })
+  }
 }
