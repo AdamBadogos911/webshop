991c95380bf925952662eb3447cb8c53ad08a2d9
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-18 12:41:51 +0100

    Product Details HTML + script

--- webshop/src/app/components/product-details/product-details.ts
+++ webshop/src/app/components/product-details/product-details.ts
@@ -1,24 +1,30 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, InjectionToken, OnInit } from '@angular/core';
 import { ReviewService } from '../../services/review-service';
 import { ActivatedRoute } from '@angular/router';
 import { ProductService } from '../../services/product-service';
-import { Product } from '../../models/product.model';
 import { Review } from '../../models/review.model';
 import { ReviewCard } from '../review-card/review-card';
+import { Product } from '../../models/product.model';
+import { ProductCard } from '../product-card/product-card';
+import { UserService } from '../../services/user-service';
+import { CartService } from '../../services/cart-service';
 
 @Component({
   selector: 'app-product-details',
-  imports: [ReviewCard],
+  imports: [ReviewCard, ProductCard],
   templateUrl: './product-details.html',
   styleUrl: './product-details.css',
 })
-export class ProductDetails implements OnInit{
+export class ProductDetails implements OnInit {
   reviewService = inject(ReviewService)
   productService = inject(ProductService)
+  userService = inject(UserService)
+  cartService = inject(CartService)
   route = inject(ActivatedRoute)
   showReviews = false
   selectedProduct!: Product
   reviews: Review[] = []
+  similarProducts: Product[] = []
 
   ngOnInit(): void {
     this.route.params.subscribe({
@@ -26,6 +32,11 @@ export class ProductDetails implements OnInit{
         this.productService.getProductById(params["productId"]).subscribe({
           next: response => {
             this.selectedProduct = response
+          },
+          complete: () => {
+            this.productService.getProductByCategory(this.selectedProduct.category.id!).subscribe({
+              next: response => this.similarProducts = response.slice(0, 4)
+            })
           }
         })
       }
@@ -45,6 +56,18 @@ export class ProductDetails implements OnInit{
     this.reviewService.getReviewsAboutProduct(this.selectedProduct.id!).subscribe({
       next: response => {
         this.reviews = response
+      }
+    })
+  }
+
+  addToCart(amount: number) {
+    if (this.userService.user == null) {
+      alert("Előbb jelentkezzél be!")
+    }
+
+    this.cartService.addProductToBasket(this.userService.user?.id!, this.selectedProduct.id!, amount).subscribe({
+      next: response => {
+        console.log(response)
       }
     })
   }
