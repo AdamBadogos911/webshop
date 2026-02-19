a11f182305d38e348506a1701da68614412eaf35
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 12:16:06 +0100

    User service update

--- webshop/src/app/services/user-service.ts
+++ webshop/src/app/services/user-service.ts
@@ -12,10 +12,22 @@ export class UserService {
   user: User | null = null;
 
   login(email: string, password: string): Observable<User> {
-    return this.http.post<User>(`${this.baseURL}/login`, {email: email, password: password});
+    return this.http.post<User>(`${this.baseURL}/login`, { email: email, password: password });
   }
 
   register(newUser: User) {
     return this.http.post<User>(`${this.baseURL}/register`, newUser);
+  }
+
+  updateUser(updatedDetails: { firstName: string, lastName: string, email: string, phoneNumber: string }) {
+    return this.http.put<User>(`${this.baseURL}/${this.user?.id}`, updatedDetails)
+  }
+
+  deleteUser() {
+    return this.http.delete(`${this.baseURL}/${this.user?.id}`)
+  }
+
+  changePfp(formData: FormData): Observable<User> {
+    return this.http.patch<User>(`${this.baseURL}/pfp/${this.user?.id}`, formData)
   }
 }
