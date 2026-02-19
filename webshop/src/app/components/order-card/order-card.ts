476733414876866ab4e94ab4f5d862599a6f285d
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 14:04:47 +0100

    Order card

--- webshop/src/app/components/order-card/order-card.ts
+++ webshop/src/app/components/order-card/order-card.ts
@@ -1,12 +1,18 @@
-import { Component, input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { OrderHistory } from '../../models/orderHistory.model';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-card',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './order-card.html',
   styleUrl: './order-card.css',
 })
-export class OrderCard {
+export class OrderCard implements OnInit{
   orderHistory = input.required<OrderHistory>()
+  transportAddress: string = ""
+
+  ngOnInit(): void {
+    this.transportAddress = this.orderHistory().orderTransportDetail?.postCode + " " + this.orderHistory().orderTransportDetail?.town + this.orderHistory().orderTransportDetail?.address + this.orderHistory().orderTransportDetail?.houseNumber
+  }
 }
