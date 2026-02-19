852d4961382a186dd3e7f0d5fbd337e2fb268ae8
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 10:16:46 +0100

    Product service update

--- webshop/src/app/services/product-service.ts
+++ webshop/src/app/services/product-service.ts
@@ -11,10 +11,15 @@ export class ProductService {
   private baseUrl = "http://localhost:8080/product"
 
   getProductByCategory(categoryId: number): Observable<Product[]> {
+    console.log(`${this.baseUrl}/category/${categoryId}?page=0&size=20`)
     return this.http.get<Product[]>(`${this.baseUrl}/category/${categoryId}?page=0&size=20`)
   }
 
   getProductById(id: number): Observable<Product> {
     return this.http.get<Product>(`${this.baseUrl}/${id}`)
+  }
+
+  getMostViewedProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}/mostViewed`)
   }
 }
