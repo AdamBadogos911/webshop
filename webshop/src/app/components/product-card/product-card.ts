7b3d45b1a9b0607d4f388c95a13034100ade0984
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 10:21:00 +0100

    Product Card bugifx

--- webshop/src/app/components/product-card/product-card.ts
+++ webshop/src/app/components/product-card/product-card.ts
@@ -1,5 +1,5 @@
 import { Component, inject, input } from '@angular/core';
-import { Product } from '../../models/product.mode';
+import { Product } from '../../models/product.model';
 import { Router } from '@angular/router';
 
 @Component({
