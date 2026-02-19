f85a35abb7e5aae1b05075ed0ca49d79abaa4595
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 21:37:20 +0100

    Profil page HTML + script update, kis design

--- webshop/src/app/components/profil-page/profil-page.ts
+++ webshop/src/app/components/profil-page/profil-page.ts
@@ -8,6 +8,7 @@ import { ReviewService } from '../../services/review-service';
 import { ReviewCard } from '../review-card/review-card';
 import { OrderService } from '../../services/order-service';
 import { OrderCard } from '../order-card/order-card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profil-page',
@@ -19,20 +20,21 @@ export class ProfilPage implements OnInit {
   userService = inject(UserService)
   reviewService = inject(ReviewService)
   orderService = inject(OrderService)
+  private router = inject(Router)
   selectedContent: "profilPage" | "orderHistory" | "reviews" = "profilPage"
   reviewList: Review[] = []
   orderHistories: OrderHistory[] = []
   addressList: AddressUser[] = []
-
   updateForm!: FormGroup
+  isEdit: boolean = false
 
   ngOnInit(): void {
     this.selectedContent = "profilPage"
     this.updateForm = new FormGroup({
-      firstName: new FormControl("", [Validators.required]),
-      lastName: new FormControl("", [Validators.required]),
-      email: new FormControl("", [Validators.required, Validators.email]),
-      phoneNumber: new FormControl("", [Validators.required])
+      firstName: new FormControl(this.userService.user?.firstName, [Validators.required]),
+      lastName: new FormControl(this.userService.user?.lastName, [Validators.required]),
+      email: new FormControl(this.userService.user?.email, [Validators.required, Validators.email]),
+      phoneNumber: new FormControl(this.userService.user?.phoneNumber, [Validators.required])
     })
   }
 
@@ -40,10 +42,10 @@ export class ProfilPage implements OnInit {
     this.selectedContent = newContent
     if (this.selectedContent == "profilPage") {
       this.updateForm = new FormGroup({
-        email: new FormControl("", [Validators.required]),
-        firstName: new FormControl("", [Validators.required]),
-        lastName: new FormControl("", [Validators.required]),
-        phoneNumber: new FormControl("", [Validators.required])
+        firstName: new FormControl(this.userService.user?.firstName, [Validators.required]),
+        lastName: new FormControl(this.userService.user?.lastName, [Validators.required]),
+        email: new FormControl(this.userService.user?.email, [Validators.required]),
+        phoneNumber: new FormControl(this.userService.user?.phoneNumber, [Validators.required])
       })
     } else if (this.selectedContent == "orderHistory") {
       this.getOrderHistory()
@@ -62,19 +64,48 @@ export class ProfilPage implements OnInit {
     this.reviewService.getReviewsByUser(this.userService.user?.id!).subscribe({
       next: response => {
         this.reviewList = response
+        console.log(response)
+        console.log(this.reviewList)
       }
     })
   }
 
   deleteUser() {
+    this.userService.deleteUser().subscribe({
 
+    })
   }
 
-  updateUser() {
+  handleUpdate() {
+    if(this.isEdit) {
+      this.userService.updateUser({
+        firstName: this.updateForm.controls["firstName"].value,
+        lastName: this.updateForm.controls["lastName"].value,
+        email: this.updateForm.controls["email"].value,
+        phoneNumber: this.updateForm.controls["phoneNumber"].value
+      }).subscribe({
+        next: response => {
+          this.userService.user = response
+        }
+      })
+    }
 
+    this.isEdit = !this.isEdit
   }
 
-  updatePfp() {
+  updatePfp(event: any) {
+    const file: File = event.target.files[0];
+     if (file) {
+      const formData = new FormData()
+      formData.append("image", file)
+      this.userService.changePfp(formData).subscribe({
+        next: response => this.userService.user = response
+      })
+    }
+  }
 
+  logout() {
+    this.userService.user = null
+    this.router.navigate(["/homePage"])
   }
 }
