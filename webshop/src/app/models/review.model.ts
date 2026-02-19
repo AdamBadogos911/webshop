0d17985710b03849ea04a5918da45dba6437821a
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-19 12:20:38 +0100

    Review model update

--- webshop/src/app/models/review.model.ts
+++ webshop/src/app/models/review.model.ts
@@ -1,8 +1,13 @@
+import { Product } from "./product.model";
+import { User } from "./user.model";
+
 export class Review {
   constructor(
     public id: number | null,
     public reviewText: string,
     public rating: number,
-    public createdAt: Date
+    public createdAt: Date,
+    public product: Product,
+    public author: User
   ) {}
 }
