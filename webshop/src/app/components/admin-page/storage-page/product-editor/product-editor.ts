257e7326871c0fd4368425e54a6ce73e3edd2775
AdamBadogos911 <badogos.adam911@gmail.com>
2026-02-27 10:02:03 +0100

    Termék módosítása a raktárban

---
+++ webshop/src/app/components/admin-page/storage-page/product-editor/product-editor.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-editor',
+  imports: [],
+  templateUrl: './product-editor.html',
+  styleUrl: './product-editor.css',
+})
+export class ProductEditor {
+
+}
