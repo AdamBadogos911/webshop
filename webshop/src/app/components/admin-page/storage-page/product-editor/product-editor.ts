6145c1a26e85ff7122e31aac1ff902c2579d40ef
AdamBadogos911 <badogos.adam911@gmail.com>
2026-03-02 09:27:51 +0100

    Product editor

--- webshop/src/app/components/admin-page/storage-page/product-editor/product-editor.ts
+++ webshop/src/app/components/admin-page/storage-page/product-editor/product-editor.ts
@@ -4,6 +4,8 @@ import { ProductService } from '../../../../services/product-service';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Brand } from '../../../../models/brand.model';
 import { OtherService } from '../../../../services/other-service';
+import { Category } from '../../../../models/category.model';
+import { CategoryService } from '../../../../services/category-service';
 
 @Component({
   selector: 'app-product-editor',
@@ -11,13 +13,15 @@ import { OtherService } from '../../../../services/other-service';
   templateUrl: './product-editor.html',
   styleUrl: './product-editor.css',
 })
-export class ProductEditor implements OnInit{
+export class ProductEditor implements OnInit {
   productService = inject(ProductService)
   otherService = inject(OtherService)
+  categoryService = inject(CategoryService)
   product = input.required<Product | null>()
   save = output<Product>()
   editorForm!: FormGroup
   brands: Brand[] = []
+  categories: Category[] = []
 
   ngOnInit(): void {
     this.otherService.getAllBrand().subscribe({
@@ -26,29 +30,81 @@ export class ProductEditor implements OnInit{
       }
     })
 
+    this.categoryService.getAllCategory().subscribe({
+      next: response => {
+        this.categories = response
+      }
+    })
+
     this.editorForm = new FormGroup({
-      name: new FormControl("", [Validators.required]),
-      brand: new FormControl("", [Validators.required]),
-      amount: new FormControl("", [Validators.required]),
-      price: new FormControl("", [Validators.required]),
-      weightInKg: new FormControl("", [Validators.required]),
-      material: new FormControl("", [Validators.required]),
-      lengthInCm: new FormControl("", [Validators.required]),
-      heightInCm: new FormControl("", [Validators.required]),
-      widthInCm: new FormControl("", [Validators.required]),
-      size: new FormControl("", [Validators.required])
+      name: new FormControl(this.product() != null ? this.product()?.name : "", [Validators.required]),
+      brand: new FormControl(this.product() != null ? this.product()?.brand.id : "", [Validators.required]),
+      amount: new FormControl(this.product() != null ? this.product()?.amount : "", [Validators.required]),
+      price: new FormControl(this.product() != null ? this.product()?.price : "", [Validators.required]),
+      weightInKg: new FormControl(this.product() != null ? this.product()?.detail.weightInKg : "", [Validators.required]),
+      material: new FormControl(this.product() != null ? this.product()?.detail.material : "", [Validators.required]),
+      lengthInCm: new FormControl(this.product() != null ? this.product()?.detail.lengthInCm : "", [Validators.required]),
+      heightInCm: new FormControl(this.product() != null ? this.product()?.detail.heightInCm : "", [Validators.required]),
+      widthInCm: new FormControl(this.product() != null ? this.product()?.detail.widthInCm : "", [Validators.required]),
+      size: new FormControl(this.product() != null ? this.product()?.detail.size : "", [Validators.required]),
+      isSet: new FormControl(this.product() != null ? this.product()?.detail.isSet : false, [Validators.required]),
+      stockKeepingUnit: new FormControl(this.product() != null ? this.product()?.stockKeepingUnit : false, [Validators.required]),
+      description: new FormControl(this.product() != null ? this.product()?.description : "", [Validators.required]),
+      category: new FormControl(this.product() != null ? this.product()?.category.id : "", [Validators.required]),
     })
   }
 
-  deleteProduct() {
-
+  saveChanges() {
+    if (this.product() == null) {
+      this.createProduct()
+    } else {
+      this.updateProduct()
+    }
   }
 
   updateProduct() {
-
+    this.productService.updateProduct(this.product()!.id! ,{
+      name: this.editorForm.controls["name"].value,
+      brandId: this.editorForm.controls["brand"].value,
+      amount: this.editorForm.controls["amount"].value,
+      price: this.editorForm.controls["price"].value,
+      weightInKg: this.editorForm.controls["weightInKg"].value,
+      material: this.editorForm.controls["material"].value,
+      lengthInCm: this.editorForm.controls["lengthInCm"].value,
+      heightInCm: this.editorForm.controls["heightInCm"].value,
+      widthInCm: this.editorForm.controls["widthInCm"].value,
+      size: this.editorForm.controls["size"].value,
+      isSet: this.editorForm.controls["isSet"].value,
+      stockKeepingUnit: this.editorForm.controls["stockKeepingUnit"].value,
+      description: this.editorForm.controls["description"].value,
+      categoryId: this.editorForm.controls["category"].value,
+    }).subscribe({
+      next: response => {
+        this.save.emit(response)
+      }
+    })
   }
-
   createProduct() {
-
+    console.log("addProduct()")
+    this.productService.addProduct({
+      name: this.editorForm.controls["name"].value,
+      brandId: this.editorForm.controls["brand"].value,
+      amount: this.editorForm.controls["amount"].value,
+      price: this.editorForm.controls["price"].value,
+      weightInKg: this.editorForm.controls["weightInKg"].value,
+      material: this.editorForm.controls["material"].value,
+      lengthInCm: this.editorForm.controls["lengthInCm"].value,
+      heightInCm: this.editorForm.controls["heightInCm"].value,
+      widthInCm: this.editorForm.controls["widthInCm"].value,
+      size: this.editorForm.controls["size"].value,
+      isSet: this.editorForm.controls["isSet"].value,
+      stockKeepingUnit: this.editorForm.controls["stockKeepingUnit"].value,
+      description: this.editorForm.controls["description"].value,
+      categoryId: this.editorForm.controls["category"].value,
+    }).subscribe({
+      next: response => {
+        this.save.emit(response)
+      }
+    })
   }
 }
