6ed9ef4a21abda18348b65f73b461d1179d06fe2
AdamBadogos911 <badogos.adam911@gmail.com>
2026-03-04 11:10:35 +0100

    Design bugfix

--- webshop/src/app/components/product-details/product-details.ts
+++ webshop/src/app/components/product-details/product-details.ts
@@ -71,7 +71,7 @@ export class ProductDetails implements OnInit {
 
   addToCart(amount: number) {
     if (this.userService.user == null) {
-      alert("Előbb jelentkezz be!")
+      alert("Előbb jelentkezzen be")
     }
 
     this.cartService.addProductToBasket(this.userService.user?.id!, this.selectedProduct.id!, amount).subscribe({
