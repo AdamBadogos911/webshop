2e8124f3b5cea9cdf231e5a5d53e2e8f84b6b137
AdamBadogos911 <badogos.adam911@gmail.com>
2026-03-15 16:51:11 +0100

    Kosár bugfix + hasonló termékek

--- webshop/src/app/components/order-page/cart/cart.ts
+++ webshop/src/app/components/order-page/cart/cart.ts
@@ -3,22 +3,33 @@ import { CartService } from '../../../services/cart-service';
 import { UserService } from '../../../services/user-service';
 import { CartCard } from './cart-card/cart-card';
 import { RouterModule } from '@angular/router';
+import { ProductService } from '../../../services/product-service';
+import { Product } from '../../../models/product.model';
+import { ProductCard } from '../../product-card/product-card';
 
 @Component({
   selector: 'app-cart',
-  imports: [CartCard, RouterModule],
+  imports: [CartCard, RouterModule, ProductCard],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
 export class CartPage implements OnInit {
   cartService = inject(CartService);
+  productService = inject(ProductService)
   userService = inject(UserService);
   sumPrice: number = 0;
+  highlightedProducts: Product[] = []
 
   ngOnInit(): void {
     this.cartService.getCartByUserId(this.userService.user?.id!).subscribe({
       next: (response) => {
         this.cartService.usersCart = response;
+
+        if (this.cartService.usersCart.cartProductList.length == 0) {
+          this.productService.getMostViewedProducts().subscribe({
+            next: response => this.highlightedProducts = response
+          })
+        }
       },
       complete: () => {
         this.calculatePrice()
