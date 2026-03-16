7c750139980c773b99be529b52c04c059d9cf885
AdamBadogos911 <badogos.adam911@gmail.com>
2026-03-16 10:05:20 +0100

    Értékelés csillag elkészítése

--- webshop/src/app/components/review-card/review-card.ts
+++ webshop/src/app/components/review-card/review-card.ts
@@ -10,8 +10,15 @@ import { Review } from '../../models/review.model';
 export class ReviewCard implements OnInit{
   review = input.required<Review>()
   parentComponent = input.required<"profilPage" | "productDetails">()
+  ratingList: number[] = []
 
   ngOnInit(): void {
-
+    for (let i : number = 1; i <= 5; i++) {
+      if (i <= this.review().rating) {
+        this.ratingList.push(1)
+      } else {
+        this.ratingList.push(0)
+      }
+    }
   }
 }
