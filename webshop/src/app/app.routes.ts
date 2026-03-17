ee1eda00ac13276d40881a271ba842fed0d1c11e
AdamBadogos911 <badogos.adam911@gmail.com>
2026-03-17 11:15:36 +0100

    App routes LazyLoading + User Guard

--- webshop/src/app/app.routes.ts
+++ webshop/src/app/app.routes.ts
@@ -1,22 +1,20 @@
 import { Routes } from '@angular/router';
+import { AboutUsPage } from './components/about-us-page/about-us-page';
+import { AdminPage } from './components/admin-page/admin-page';
 import { HomePage } from './components/home-page/home-page';
 import { LoginPage } from './components/login-page/login-page';
-import { RegistrationPage } from './components/registration-page/registration-page';
-import { PasswordResetPage } from './components/password-reset-page/password-reset-page';
 import { NotFound } from './components/not-found/not-found';
-import { StoragePage } from './components/admin-page/storage-page/storage-page';
-import { OrderHistoryPage } from './components/admin-page/order-history-page/order-history-page';
-import { AdminPage } from './components/admin-page/admin-page';
-import { ProfilPage } from './components/profil-page/profil-page';
-import { ProductDetails } from './components/product-details/product-details';
-import { Unauthorized } from './components/unauthorized/unauthorized';
-import { AboutUsPage } from './components/about-us-page/about-us-page';
-import { StatisticsPage } from './components/admin-page/statistics-page/statistics-page';
-import { OrderPage } from './components/order-page/order-page';
-import { CartPage } from './components/order-page/cart/cart';
-import { TransportDetails } from './components/order-page/transport-details/transport-details';
 import { BillingDetails } from './components/order-page/billing-details/billing-details';
+import { CartPage } from './components/order-page/cart/cart';
+import { OrderPage } from './components/order-page/order-page';
 import { SummaryPage } from './components/order-page/summary-page/summary-page';
+import { TransportDetails } from './components/order-page/transport-details/transport-details';
+import { PasswordResetPage } from './components/password-reset-page/password-reset-page';
+import { ProfilPage } from './components/profil-page/profil-page';
+import { RegistrationPage } from './components/registration-page/registration-page';
+import { Unauthorized } from './components/unauthorized/unauthorized';
+import { UserGuard } from './routeGuards/userGuard';
+import { AdminGuard } from './routeGuards/adminGuard';
 
 export const routes: Routes = [
   { path: "homePage", component: HomePage, },
@@ -24,18 +22,13 @@ export const routes: Routes = [
   { path: "login", component: LoginPage },
   { path: "register", component: RegistrationPage },
   { path: "passwordReset", component: PasswordResetPage },
-  { path: "productList/:categoryId", loadComponent: () => import("./components/product-list/product-list").then((c) => c.ProductList) },
   { path: "aboutUs", component: AboutUsPage },
   { path: "unauthorized", component: Unauthorized },
-  { path: "profile", component: ProfilPage },
-  { path: "adminPage", component: AdminPage },
-  { path: "orderHistoryPage", component: OrderHistoryPage},
-  { path: "storagePage", component: StoragePage},
-  { path: "productDetails/:productId", component: ProductDetails },
-  { path: "statisticsPage", component: StatisticsPage},
+  { path: "profile", component: ProfilPage, canActivate: [UserGuard] },
+  { path: "adminPage", component: AdminPage, canActivate: [UserGuard, AdminGuard] },
 
   {
-    path: "orderPage", component: OrderPage, children: [
+    path: "orderPage", component: OrderPage, canActivate: [UserGuard], children: [
       { path: "cart", component: CartPage },
       { path: "transportDetails", component: TransportDetails },
       { path: "billingDetails", component: BillingDetails },
@@ -43,5 +36,11 @@ export const routes: Routes = [
     ]
   },
 
+  //lazyLoading:
+  { path: "productList/:categoryId", loadComponent: () => import("./components/product-list/product-list").then((c) => c.ProductList) },
+  { path: "productDetails/:productId", loadComponent: () => import("./components/product-details/product-details").then((c) => c.ProductDetails) },
+  { path: "orderHistoryPage", canActivate: [AdminGuard], loadComponent: () => import("./components/admin-page/order-history-page/order-history-page").then((c) => c.OrderHistoryPage) },
+  { path: "storagePage", canActivate: [AdminGuard], loadComponent: () => import("./components/admin-page/storage-page/storage-page").then((c) => c.StoragePage) },
+  { path: "statisticsPage", canActivate: [AdminGuard], loadComponent: () => import("./components/admin-page/statistics-page/statistics-page").then((c) => c.StatisticsPage) },
   { path: "**", component: NotFound },
 ];
