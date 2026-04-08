533fc2c7ac104595a4466bf8ed22b10db0376267
AdamBadogos911 <badogos.adam911@gmail.com>
2026-04-08 22:21:20 +0200

    Statisztika Admin oldal

--- webshop/src/app/components/admin-page/statistics-page/statistics-page.ts
+++ webshop/src/app/components/admin-page/statistics-page/statistics-page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { OrderService } from '../../../services/order-service';
+import { ProductService } from '../../../services/product-service';
+import { Product } from '../../../models/product.model';
 
 @Component({
   selector: 'app-statistics-page',
@@ -6,6 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './statistics-page.html',
   styleUrl: './statistics-page.css',
 })
-export class StatisticsPage {
+export class StatisticsPage implements OnInit {
+  orderService = inject(OrderService)
+  productService = inject(ProductService)
+  statistic: { income: number, numberOfOrder: number, averageOrderedPrice: number, numberOfSoldProduct: number } | null = null
+  mostViewedProduct: Product[] = []
 
+  ngOnInit(): void {
+    this.orderService.getStatistic().subscribe({
+      next: response => this.statistic = response
+    })
+
+    this.productService.getMostViewedProducts().subscribe({
+      next: response => this.mostViewedProduct = response
+    })
+  }
 }
