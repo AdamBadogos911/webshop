e525bf944be39003f7eed584f647c39d7b1d51fd
AdamBadogos911 <badogos.adam911@gmail.com>
2026-04-26 14:46:03 +0200

    App config update

--- webshop/src/app/app.config.ts
+++ webshop/src/app/app.config.ts
@@ -3,7 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
+import { AuthInterceptor } from './interceptors/AuthInterceptor';
+import { CookieSetterInterceptor } from './interceptors/CookieSetterInterceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -11,7 +13,8 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay()),
     provideHttpClient(
-      withFetch()
+      withFetch(),
+      withInterceptors([AuthInterceptor, CookieSetterInterceptor])
     )
   ]
 };
